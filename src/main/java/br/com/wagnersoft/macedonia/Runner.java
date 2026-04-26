95422fd543fd8e96ac17da1f2732907874ff5594
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-04-26 08:55:08 -0300

    Added resource

--- src/main/java/br/com/wagnersoft/macedonia/Runner.java
+++ src/main/java/br/com/wagnersoft/macedonia/Runner.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Runner {
 
-	@RequestMapping("/")
+	@RequestMapping("/init")
 	String home() {
 		return "Runnig...";
 	}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module macedonia {
-	requires jakarta.persistence;
-	requires lombok;
-	requires spring.boot;
-	requires spring.boot.autoconfigure;
-	requires spring.web;
-}
