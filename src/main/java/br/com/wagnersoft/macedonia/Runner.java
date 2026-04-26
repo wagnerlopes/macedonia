a8d5f654d0f8388bb35e5f6beacfa40359f7527d
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-04-26 09:50:07 -0300

    Templates added

--- src/main/java/br/com/wagnersoft/macedonia/Runner.java
+++ src/main/java/br/com/wagnersoft/macedonia/Runner.java
@@ -2,16 +2,18 @@ package br.com.wagnersoft.macedonia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class Runner {
 
-	@RequestMapping("/")
-	String home() {
-		return "Runnig...";
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("name", "Rei Lear");
+		return "home";
 	}
 
 	public static void main(String[] args) {
