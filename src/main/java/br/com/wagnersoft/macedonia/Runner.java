5ceace42b885af1c9b0eeab25f378268eb4ee8b4
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-04-27 17:14:49 -0300

    correção de codigo

--- src/main/java/br/com/wagnersoft/macedonia/Runner.java
+++ src/main/java/br/com/wagnersoft/macedonia/Runner.java
@@ -2,11 +2,11 @@ package br.com.wagnersoft.macedonia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class Runner {
 
