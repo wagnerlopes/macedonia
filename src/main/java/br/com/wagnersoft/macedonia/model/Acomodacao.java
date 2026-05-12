88399805e1250a42e88ae37cd447570de26222bd
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-05-12 16:12:56 -0300

    Contrato save updated

--- src/main/java/br/com/wagnersoft/macedonia/model/Acomodacao.java
+++ src/main/java/br/com/wagnersoft/macedonia/model/Acomodacao.java
@@ -2,42 +2,49 @@ package br.com.wagnersoft.macedonia.model;
 
 public enum Acomodacao {
 
-	SUITE_INDIVIDUAL("Suíte individual"), 
-	QUARTO_INDIVIDUAL("Quarto individual"), 
-	ENFERMARIA_6("Enfermaria 6 leitos"), 
-	ENFERMARIA_8("Enfermaria 8 leitos");
+	SUITE_INDIVIDUAL(1, "Suíte individual"),
+	QUARTO_INDIVIDUAL(2, "Quarto individual"),
+	ENFERMARIA_6(3, "Enfermaria 6 leitos"),
+	ENFERMARIA_8(4, "Enfermaria 8 leitos");
 
 	public static final Acomodacao[] ALL = { SUITE_INDIVIDUAL, QUARTO_INDIVIDUAL, ENFERMARIA_6, ENFERMARIA_8 };
 
-	private final String name;
+	private final int codigo;
 
-	public static Acomodacao forName(final String name) {
-		if (name == null) {
+	private final String descricao;
+
+	public static Acomodacao forName(final String descricao) {
+		if (descricao == null) {
 			throw new IllegalArgumentException("Nome não pode ser nulo para acomodação");
 		}
-		if (name.toUpperCase().equals("Suíte individual")) {
+		if (descricao.toUpperCase().equals("Suíte individual")) {
 			return SUITE_INDIVIDUAL;
-		} else if (name.toUpperCase().equals("Quarto individual")) {
+		} else if (descricao.toUpperCase().equals("Quarto individual")) {
 			return QUARTO_INDIVIDUAL;
-		} else if (name.toUpperCase().equals("Enfermaria 6 leitos")) {
+		} else if (descricao.toUpperCase().equals("Enfermaria 6 leitos")) {
 			return ENFERMARIA_6;
-		} else if (name.toUpperCase().equals("Enfermaria 8 leitos")) {
+		} else if (descricao.toUpperCase().equals("Enfermaria 8 leitos")) {
 			return ENFERMARIA_8;
 		}
-		throw new IllegalArgumentException("Nome \"" + name + "\" não corresponde a nenhuma acomodação");
+		throw new IllegalArgumentException("Descrição \"" + descricao + "\" não corresponde a nenhuma acomodação");
 	}
 
-	private Acomodacao(final String name) {
-		this.name = name;
+	private Acomodacao(final int codigo, final String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
 	}
 
-	public String getName() {
-		return this.name;
+	public int getCodigo() {
+		return this.codigo;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
 	}
 
 	@Override
 	public String toString() {
-		return getName();
+		return getDescricao();
 	}	
 
 }
