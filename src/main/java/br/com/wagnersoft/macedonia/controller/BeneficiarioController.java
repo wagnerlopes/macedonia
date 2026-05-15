9e469ae03d2de0536fb04d6b365f72d9c94ebf43
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-05-15 11:19:54 -0300

    Beneficiarios Controller saved

--- src/main/java/br/com/wagnersoft/macedonia/controller/BeneficiarioController.java
+++ src/main/java/br/com/wagnersoft/macedonia/controller/BeneficiarioController.java
@@ -1,5 +1,7 @@
 package br.com.wagnersoft.macedonia.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.wagnersoft.macedonia.model.Beneficiario;
@@ -26,18 +29,22 @@ public class BeneficiarioController {
         super();
     }
     
+    @ModelAttribute("allBeneficiarios")
+    public List<Beneficiario> populateBeneficiarios() {
+        return benSvc.listAll();
+    }
+    
     @GetMapping({"/beneficiarios"})
     public String show(final Beneficiario beneficiario, Model model) {
 		logger.info("+++ Beneficiarios +++");
 		model.addAttribute("menu", "ben");
-		model.addAttribute("lista", benSvc.listAll());
         return "beneficiarios";
     }
     
     @PostMapping(value="/beneficiarios/save", params={"save"})
     public String save(@Valid final Beneficiario beneficiario, final BindingResult bindingResult, final ModelMap model) {
         if (bindingResult.hasErrors()) {
-            return "redirect:/beneficiarios";
+            return "beneficiarios";
         }
         logger.info("{}", beneficiario);
         benSvc.add(beneficiario);
