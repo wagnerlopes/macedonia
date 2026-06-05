d02fd9bb18a2883afa23dcd4e271993c91635aef
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-06-05 10:22:52 -0300

    Index updated

--- src/main/java/br/com/wagnersoft/macedonia/Runner.java
+++ src/main/java/br/com/wagnersoft/macedonia/Runner.java
@@ -1,19 +1,13 @@
 package br.com.wagnersoft.macedonia;
 
-import java.time.Duration;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import br.com.wagnersoft.macedonia.service.CboService;
 import jakarta.servlet.http.HttpSession;
@@ -27,14 +21,6 @@ public class Runner {
 	@Autowired
 	private CboService cboSvc;
 
-	@Bean
-	public LocaleResolver localeResolver() {
-	    final CookieLocaleResolver resolver = new CookieLocaleResolver("LOCALE");
-	    resolver.setDefaultLocale(Locale.of("pt", "BR"));
-	    resolver.setCookieMaxAge(Duration.ofSeconds(3600));
-	    return resolver;
-	}
-	
 	@GetMapping("/")
 	public String index(HttpSession session, Model model) {
 		logger.info("+++ Index +++");
