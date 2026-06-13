e214951d70b638b807f7449c3767ecbae5f65f5b
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-06-13 13:50:24 -0300

    Main (#13)
    * Guias de Encaminhamento Finalizadas (#12)
    
    * Index views saved
    
    * DTH controller, service and views saved
    
    * Global Excpetion Handler saved
    
    * Beneficiarios error saved
    
    * Contratos error saved
    
    * DTH controller saved
    
    * DTH controller saved
    
    * Contrato controller saved
    
    * Config saved
    
    * Guia html saved
    
    * Guia controller, service, model saved
    
    * Beneficiario & Profissional controller saved
    
    * COntrato e DTH controller saved
    
    * Validacao incluida
    
    * Validacao incluida
    
    * OCS-PM incluida
    
    * OCS, OCSPM view saved
    
    * View templates saved
    
    * i18N added
    
    * Index updated
    
    * Index updated
    
    * Index updated
    
    * View templates saved
    
    * Contrato model changed
    
    * Messages updated
    
    * Model GuiaPm updated
    
    * GuiaEncaminhamento updated
    
    * Update project name and features in README
    
    Inclusão de informações relevantes sobre o projeto.
    
    * GuiaEncaminhamento updated
    
    * Guia view updated
    
    * Guia calculo de total parcial e geral
    
    * Guia calculo de total parcial e geral
    
    * Tiss service updated
    
    * Versão Testes (#8) (#9)
    
    * Index views saved
    
    * DTH controller, service and views saved
    
    * Global Excpetion Handler saved
    
    * Beneficiarios error saved
    
    * Contratos error saved
    
    * DTH controller saved
    
    * DTH controller saved
    
    * Contrato controller saved
    
    * Config saved
    
    * Guia html saved
    
    * Guia controller, service, model saved
    
    * Beneficiario & Profissional controller saved
    
    * COntrato e DTH controller saved
    
    * Validacao incluida
    
    * Validacao incluida
    
    * OCS-PM incluida
    
    * OCS, OCSPM view saved
    
    * View templates saved
    
    * i18N added
    
    * Index updated
    
    * Index updated
    
    * Index updated
    
    * View templates saved
    
    * Contrato model changed
    
    * Messages updated
    
    * Model GuiaPm updated
    
    * GuiaEncaminhamento updated
    
    * Update project name and features in README
    
    Inclusão de informações relevantes sobre o projeto.
    
    * GuiaEncaminhamento updated
    
    * Guia view updated
    
    * Guia calculo de total parcial e geral
    
    * Guia calculo de total parcial e geral
    
    * Tiss service updated
    
    * Guia controller & service updated
    
    * Guia controller & service updated
    
    * Contrato controller & view updated
    
    * DTH controller & view updated
    
    * Guias controller & view updated
    
    * OCS controller & view updated
    
    * OcsPm controller & service updated
    
    * Profissional controller & service updated
    
    * Protocolo service updated
    
    * Guias Ocs-Pm finalized
    
    * Adicionado Bean para h2-console e versao 2.4.240 no pom.xml

--- src/main/java/br/com/wagnersoft/macedonia/Runner.java
+++ src/main/java/br/com/wagnersoft/macedonia/Runner.java
@@ -4,36 +4,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.Servlet;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @SpringBootApplication
 public class Runner {
 
-	private static final Logger logger = LoggerFactory.getLogger(Runner.class);
+  private static final Logger logger = LoggerFactory.getLogger(Runner.class);
 
-	@GetMapping("/")
-	public String index(HttpSession session, Model model) {
-		logger.info("+++ Index +++");
-		session.setMaxInactiveInterval(0);
-		session.setAttribute("usr", " Meu Truta ");
-		model.addAttribute("menu", "index");
-		return "index";
-	}
-	
-	@GetMapping("/configuracoes")
-	public String configuracoes(Model model) {
-		logger.info("+++ Config +++");
-		model.addAttribute("menu", "config");
-		return "configuracoes";
-	}
+  @Bean
+  public ServletRegistrationBean<Servlet> h2ServletRegistration() {
+    final Servlet servlet = new org.h2.server.web.JakartaWebServlet();
+    logger.info("{}", servlet);
+    return new ServletRegistrationBean<>(servlet, "/h2-console/*");
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Runner.class, args);
-	}
+  @GetMapping("/")
+  public String index(HttpSession session, Model model) {
+    logger.info("+++ Index +++");
+    session.setMaxInactiveInterval(0);
+    session.setAttribute("usr", " Meu Truta ");
+    model.addAttribute("menu", "index");
+    return "index";
+  }
+
+  @GetMapping("/configuracoes")
+  public String configuracoes(Model model) {
+    logger.info("+++ Config +++");
+    model.addAttribute("menu", "config");
+    return "configuracoes";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Runner.class, args);
+  }
 
 }
