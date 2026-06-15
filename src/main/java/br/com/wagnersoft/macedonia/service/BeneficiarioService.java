a37f519b8e09a543867992acef2dfa7f5747af43
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-06-15 15:43:53 -0300

    Beneficiario controller & service updated

--- src/main/java/br/com/wagnersoft/macedonia/service/BeneficiarioService.java
+++ src/main/java/br/com/wagnersoft/macedonia/service/BeneficiarioService.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.wagnersoft.macedonia.model.Beneficiario;
@@ -23,43 +24,45 @@ import br.com.wagnersoft.macedonia.repository.BeneficiarioRepository;
 @Service
 public class BeneficiarioService {
 
-	private static final Logger logger = LoggerFactory.getLogger(BeneficiarioService.class);
+  private static final Logger logger = LoggerFactory.getLogger(BeneficiarioService.class);
 
-	@Autowired
-	private BeneficiarioRepository rep;
+  @Autowired
+  private BeneficiarioRepository rep;
 
-	public Optional<Beneficiario> findByCpf(String cpf) {
-		return rep.findById(cpf);
-	}
-	
-	public List<Beneficiario> listAll() {
-		final List<Beneficiario> lista = rep.findAll();
-		lista.forEach(e -> {logger.debug(e.toString());});
-		return lista;    }
-	/** Usado nas listas onde interessa apenas o nome (value) e o cpf (key).
-	 * @return
-	 */
-	public Map<String, String> mapAll() {
-		return listAll().stream()
-		    .sorted(Comparator.comparing(Beneficiario::getNome, String.CASE_INSENSITIVE_ORDER))
-		    .collect(Collectors.toMap(Beneficiario::getCpf, Beneficiario::getNome, (existing, replacement) -> existing, LinkedHashMap::new));
-	}
+  public Optional<Beneficiario> findByCpf(final String cpf) {
+    if (cpf == null || cpf.isBlank()) return Optional.empty();
+    return rep.findById(cpf.replaceAll("[^0-9]", ""));
+  }
 
-	public void remove(final String cpf) {
-		 rep.findById(cpf).ifPresent(b -> rep.delete(b));
-	}
-	
-	public void add(final Beneficiario beneficiario) {
-	  if (beneficiario == null) return;
-	  Optional.ofNullable(beneficiario.getCpf())
-      .flatMap(rep::findById)
-      .ifPresentOrElse(existing -> this.save(existing, beneficiario), () -> rep.save(beneficiario));
-	}
+  public List<Beneficiario> listAll() {
+    final List<Beneficiario> lista = rep.findAll(Sort.by(Sort.Order.by("nome").ignoreCase()));
+    logger.debug("{}", lista);
+    return lista;  }
+  /** Retorna apenas o mapa de nome (value) e o cpf (key) usado em select list.
+   * @return {@link Map<String, String>} CPF e Valor
+   */
+  public Map<String, String> mapAll() {
+    return listAll().stream()
+        .sorted(Comparator.comparing(Beneficiario::getNome, String.CASE_INSENSITIVE_ORDER))
+        .collect(Collectors.toMap(Beneficiario::getCpf, Beneficiario::getNome, (existing, replacement) -> existing, LinkedHashMap::new));
+  }
 
-	private void save(final Beneficiario existing, final Beneficiario newBen) {
-		existing.setNome(newBen.getNome());
-		existing.setNascimentoData(newBen.getNascimentoData());
-		rep.save(existing);
-	}
-	
+  public void remove(final String cpf) {
+    if (cpf == null || cpf.isBlank()) return;
+    rep.findById(cpf.replaceAll("[^0-9]","")).ifPresent(b -> rep.delete(b));
+  }
+
+  public void add(final Beneficiario beneficiario) {
+    if (beneficiario == null) return;
+    Optional.ofNullable(beneficiario.getCpf())
+    .flatMap(rep::findById)
+    .ifPresentOrElse(existing -> this.save(existing, beneficiario), () -> rep.save(beneficiario));
+  }
+
+  private void save(final Beneficiario existing, final Beneficiario replacement) {
+    existing.setNome(replacement.getNome());
+    existing.setNascimentoData(replacement.getNascimentoData());
+    rep.save(existing);
+  }
+
 }
