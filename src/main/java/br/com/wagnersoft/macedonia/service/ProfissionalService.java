76d22f72b07106cede3708468e9f82cf308e74f0
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-06-16 08:02:01 -0300

    Profissional service updated

--- src/main/java/br/com/wagnersoft/macedonia/service/ProfissionalService.java
+++ src/main/java/br/com/wagnersoft/macedonia/service/ProfissionalService.java
@@ -1,5 +1,6 @@
 package br.com.wagnersoft.macedonia.service;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -8,12 +9,13 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.wagnersoft.macedonia.model.Profissional;
 import br.com.wagnersoft.macedonia.repository.ProfissionalRepository;
 
-/** Profissional service.
+/** Profissional de Saude service.
  * @since 1.0
  * @version 1.0
  * @author Wagner Lopes
@@ -24,40 +26,48 @@ public class ProfissionalService {
 	private static final Logger logger = LoggerFactory.getLogger(ProfissionalService.class);
 
 	@Autowired
-	private ProfissionalRepository profRep;
+	private ProfissionalRepository rep;
 
-	public Optional<Profissional> findByCpf(String cpf) {
-		return profRep.findById(cpf);
+	public Optional<Profissional> findByCpf(final String cpf) {
+    if (cpf == null || cpf.isBlank()) return Optional.empty();
+    return rep.findById(cpf.replaceAll("[^0-9]", ""));
 	}
 
 	public List<Profissional> listAll() {
-		final List<Profissional> lista = profRep.findAll();
-		lista.forEach(e -> {logger.debug(e.toString());});
-		return lista;
+    final List<Profissional> lista = rep.findAll(Sort.by(Sort.Order.by("nome").ignoreCase()));
+    logger.debug("{}", lista);
+    return lista;
 	}
 
+  /** Retorna apenas o mapa de nome (value) e o cpf (key) usado em select list.
+   * @return {@link Map<String, String>} CPF e Nome
+   */
 	public Map<String, String> mapAll() {
-		return listAll().stream().collect(Collectors.toMap(Profissional::getCpf, Profissional::getNome));
+    return this.listAll().stream()
+        .collect(Collectors.toMap(Profissional::getCpf, Profissional::getNome, (existing, replacement) -> existing, LinkedHashMap::new));
 	}
 	
-	public void add(Profissional profissional) {
-		profRep.findById(profissional.getCpf()).ifPresentOrElse(oldProf -> save(oldProf, profissional), () -> profRep.save(profissional));;
-		profRep.save(profissional);
-	}
-
-	public void remove(String cpf) {
-		profRep.findById(cpf).ifPresent(p -> {
+	public void remove(final String cpf) {
+    if (cpf == null || cpf.isBlank()) return;
+		rep.findById(cpf.replaceAll("[^0-9]","")).ifPresent(p -> {
 			if (p.getGuiasResponsavel().isEmpty() && p.getGuiasSolicitante().isEmpty())
-				profRep.delete(p);
+				rep.delete(p);
 		});
 	}
 
-	private void save(final Profissional oldProf, final Profissional newProf) {
-		oldProf.setNome(newProf.getNome());
-		oldProf.setCbo(newProf.getCbo());
-		oldProf.setCns(newProf.getCns());
-		oldProf.setRegistroProfissional(newProf.getRegistroProfissional());
-		profRep.save(oldProf);
+  public void add(final Profissional profissional) {
+    if (profissional == null) return;
+    Optional.ofNullable(profissional.getCpf())
+      .flatMap(rep::findById)
+      .ifPresentOrElse(existing -> this.save(existing, profissional), () -> rep.save(profissional));
+  }
+
+	private void save(final Profissional existing, final Profissional replacement) {
+		existing.setNome(replacement.getNome());
+		existing.setCbo(replacement.getCbo());
+		existing.setCns(replacement.getCns());
+		existing.setRegistroProfissional(replacement.getRegistroProfissional());
+		rep.save(existing);
 	}
 	
 }
