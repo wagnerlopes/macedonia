3b73f669ebf33c453caa6102bcba3e948465b892
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-06-22 19:13:57 -0300

    messages_en_US added

---
+++ src/main/java/br/com/wagnersoft/macedonia/I18nConfig.java
@@ -0,0 +1,33 @@
+package br.com.wagnersoft.macedonia;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class I18nConfig implements WebMvcConfigurer {
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    CookieLocaleResolver resolver = new CookieLocaleResolver();
+    resolver.setDefaultLocale(java.util.Locale.forLanguageTag("pt-BR"));
+    return resolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+    interceptor.setParamName("lang"); // ?lang=pt_BR, ?lang=en_US, etc.
+    return interceptor;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+}
