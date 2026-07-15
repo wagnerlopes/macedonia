344033d1fc8e59c2ae66a80d78b682f6fa0dcd53
Wagner Lopes <wagnerlopes@gmail.com>
2026-07-15 08:59:29 -0300

    Test 1 updated

--- src/test/java/br/com/wagnersoft/macedonia/StringToBigDecimalConverterTest.java
+++ src/test/java/br/com/wagnersoft/macedonia/StringToBigDecimalConverterTest.java
@@ -1,6 +1,7 @@
 package br.com.wagnersoft.macedonia;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
@@ -14,8 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class StringToBigDecimalConverterTest {
 
   @Test
-  @DisplayName("Deve retornar BigDecimal")
-  void deveRetornarBigDecimal() {
+  @DisplayName("String x.xxx,xx Deve retornar BigDecimal")
+  void pontoVirguladeveRetornarBigDecimal() {
     // Arrange
     String decimal = "1.230,89";
     
@@ -25,6 +26,47 @@ public class StringToBigDecimalConverterTest {
     // Assert
     assertNotNull(result);
     assertEquals(new BigDecimal("1230.89"), result);
+  }
+
+  @Test
+  @DisplayName("String xxx,xx Deve retornar BigDecimal")
+  void virgulaDeveRetornarBigDecimal() {
+    // Arrange
+    String decimal = "1230,89";
+    
+    // Act
+    BigDecimal result = new StringToBigDecimalConverter().convert(decimal);
+    
+    // Assert
+    assertNotNull(result);
+    assertEquals(new BigDecimal("1230.89"), result);
+  }
+
+  @Test
+  @DisplayName("String ABCD Deve retornar NULL")
+  void stringDeveRetornarNull() {
+    // Arrange
+    String decimal = "ABCD";
+    BigDecimal result = new StringToBigDecimalConverter().convert(decimal);
+    assertNull(result);
+  }
+
+  @Test
+  @DisplayName("String NULL Deve retornar NULL")
+  void nuloDeveRetornarNull() {
+    // Arrange
+    String decimal = null;
+    BigDecimal result = new StringToBigDecimalConverter().convert(decimal);
+    assertNull(result);
+  }
+
+  @Test
+  @DisplayName("String vazia Deve retornar NULL")
+  void vazioRetornarNull() {
+    // Arrange
+    String decimal = "";
+    BigDecimal result = new StringToBigDecimalConverter().convert(decimal);
+    assertNull(result);
   }
   
 }
